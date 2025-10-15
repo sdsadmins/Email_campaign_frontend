ee4c0ebc71538d525d6900e2656832cfb3f58b17
mahanaatma1 <tusharkumar28380@gmail.com>
2025-10-15 10:45:54 +0530

    Update Base url

--- src/actions/BASE_URL.js
+++ src/actions/BASE_URL.js
@@ -1,2 +1,2 @@
 // Base URL for API endpoints
-export const BASE_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3000/api/v1';
+export const BASE_URL = process.env.NEXT_PUBLIC_API_URL || 'http://195.35.23.190:5012/api/v1';
